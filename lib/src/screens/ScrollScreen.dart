149dcca00ab89d8f0d84641a7950640fc7387d39
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-02-28 21:40:22 -0500

    135. diseño scroll parte 2 - con safe area y assets

--- lib/src/screens/ScrollScreen.dart
+++ lib/src/screens/ScrollScreen.dart
@@ -8,15 +8,56 @@ class ScrollScreen extends StatelessWidget {
         scrollDirection: Axis.vertical,
         children: [
           _pagina1(),
-          _pagina2()
+          _pagina2(),
+
         ],
       ),
     );
   }
 
   Widget _pagina1(){
-    return Center(
-      child: Text('Pagina 1'),
+    return Stack(
+      children: [
+        _colorFondo(),
+        _imagenFondo(),
+        _texto()
+      ],
+    );
+  }
+
+  Widget _colorFondo(){
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _imagenFondo(){
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/img/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _texto(){
+    final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox(height: 20.0,),
+          Text('11°', style: estiloTexto,),
+          Text('Miercoles', style: estiloTexto,),
+          Expanded(
+            child: Container(),
+          ),
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white,)
+        ],
+      ),
     );
   }
 
